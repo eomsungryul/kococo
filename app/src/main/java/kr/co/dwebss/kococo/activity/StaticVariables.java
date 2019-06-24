bb3a433182d16ffc7d6d09758c22971d38cfccf4
yr.seo <yr.seo@dwebss.co.kr>
2019-06-24 15:38:48 +0900

    버전 체크 및 업데이트 수정

---
+++ app/src/main/java/kr/co/dwebss/kococo/activity/StaticVariables.java
@@ -0,0 +1,9 @@
+package kr.co.dwebss.kococo.activity;
+
+public class StaticVariables {
+    public static String version = "";
+    public static int size = 0;
+    public static int patchCnt = 0;
+    public static boolean patchDownloadSuccessful = false;
+    public static boolean patchDownloadInProgress = false;
+}
