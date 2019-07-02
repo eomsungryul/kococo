100f02e1d92b81ac2144291d570b57931de8d409
eomsungryul <sr.eom@dwebss.co.kr>
2019-07-02 15:16:16 +0900

    전문가 상담하기 상담하기 클릭 시 분석 파일들 업로드 기능 - 삭제된 데이터가 있을 경우 삭제된 데이터가 있는데 상담을 진행 하시겠습니까? 라는  다이얼로그를 띄움

--- app/src/main/java/kr/co/dwebss/kococo/activity/ConsultActivity.java
+++ app/src/main/java/kr/co/dwebss/kococo/activity/ConsultActivity.java
@@ -15,11 +15,14 @@
  */
 package kr.co.dwebss.kococo.activity;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -36,6 +39,11 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
@@ -44,8 +52,13 @@ import com.google.gson.JsonParser;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import kr.co.dwebss.kococo.R;
 import kr.co.dwebss.kococo.http.ApiService;
+import kr.co.dwebss.kococo.util.FindAppIdUtil;
 import okhttp3.MediaType;
 import okhttp3.RequestBody;
 import retrofit2.Call;
@@ -71,7 +84,9 @@ public class ConsultActivity extends AppCompatActivity {
     EditText consultTitle;
 
     ScrollView sv;
+    int uploadFileCnt=0;
 
+    ProgressDialog uploadDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -188,8 +203,9 @@ public class ConsultActivity extends AppCompatActivity {
                     new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
 //                        Toast.makeText(getApplicationContext(),"예를 선택했습니다.",Toast.LENGTH_LONG).show();
-                            Toast.makeText(getApplicationContext(),"등록중입니다 잠시만기다려주세요. ",Toast.LENGTH_LONG).show();
-                            addConsult();
+//                            Toast.makeText(getApplicationContext(),"등록중입니다 잠시만기다려주세요. ",Toast.LENGTH_LONG).show();
+                            chechUploadFile();
+//                            addConsult();
                         }
                     });
             builder.setNegativeButton("아니오",
@@ -202,6 +218,123 @@ public class ConsultActivity extends AppCompatActivity {
             //이런식으로 높이와 길이를 지정할수있지만 비율에 맞게 버튼위치가 늘어나지않음 비추.
 //        builder.show().getWindow().setLayout(600, 400);
         }
+    }
+
+
+    private void chechUploadFile() {
+        boolean deleteFlag = true;
+        JsonArray uploadList = new JsonArray();
+        for(int i = 0; i<analysisList.size(); i++){
+            JsonObject obj = analysisList.get(i).getAsJsonObject();
+            String filePath = obj.get("analysisServerUploadPath").getAsString();
+            File file = new File(filePath);
+            if(file.exists()){
+                uploadList.add(analysisList.get(i));
+            }else{
+                deleteFlag = false;
+            }
+        }
+
+        if(!deleteFlag){
+            //파일이 없는 경우 삭제된 데이터가 있는데 상담을 진행 하겠습니까? 라는 것을 띄운다.
+            AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.MyAlertDialogStyle);
+            builder.setTitle("상담하기");
+            builder.setMessage("삭제된 데이터가 있는데 상담을 진행 하겠습니까?");
+            //setView()를 이용하여 view를 넣고 커스텀 할 수 있다.
+            //예일 경우에는 상담하기를 한다.
+            //1. 파이어베이스에 업로드를 한다.
+            //2. 업로드가 되면 상담하기 제출을 한다.
+            builder.setPositiveButton("예",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            uploadAllFile(uploadList);
+                        }
+                    });
+            builder.setNegativeButton("아니오",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            return;
+                        }
+                    });
+            builder.show();
+        }else{
+            uploadAllFile(uploadList);
+        }
+
+    }
+
+
+    private void uploadAllFile(JsonArray uploadList) {
+        //업로드 파일 시작
+        FindAppIdUtil fau = new FindAppIdUtil();
+        String appId =  fau.getAppid(getApplicationContext());
+
+        //firebase 업로드 관련
+        // 가장 먼저, FirebaseStorage 인스턴스를 생성한다
+        // getInstance() 파라미터에 들어가는 값은 firebase console에서
+        // storage를 추가하면 상단에 gs:// 로 시작하는 스킴을 확인할 수 있다
+        FirebaseStorage storage = FirebaseStorage.getInstance("gs://kococo-2996f.appspot.com/");
+
+        //위에서 생성한 FirebaseStorage 를 참조하는 storage를 생성한다
+        StorageReference storageRef = storage.getReference();
+        // 위의 저장소를 참조하는 images폴더안의 space.jpg 파일명으로 지정하여
+        // 하위 위치를 가리키는 참조를 만든다
+        // 이부분은 firebase 스토리지 쪽에 업로드 되는 경로이다.
+        //그렇기에 폴더 규칙을 재생데이터/앱아이디/일별날짜/파일 이런식으로 작성
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+//        {"analysisId":1943,"analysisServerUploadPath":"/data/user/0/kr.co.dwebss.kococo/files/rec_data/633/snoring-201907_02_1118~02_1121_1562034101327.mp3"}
+
+        //내 실제 경로를 입력한다.
+        uploadFileCnt= 0;
+        uploadDialog = new ProgressDialog(this,R.style.MyAlertDialogStyle);
+        uploadDialog.setMessage("파일 전송중입니다. ("+uploadFileCnt+"/"+uploadList.size()+")");
+        uploadDialog.show();
+
+        for(int i = 0; i<uploadList.size(); i++){
+
+            JsonObject obj = uploadList.get(i).getAsJsonObject();
+            String filePath = obj.get("analysisServerUploadPath").getAsString();
+            String[] filePathSplit = filePath.split("/");
+            String fileNm = filePathSplit[filePathSplit.length-1];
+
+            String uploadFirebasePath = "rec_data/"+appId+"/"+sdf.format(date)+"/"+fileNm;
+            StorageReference spaceRef = storageRef.child(uploadFirebasePath);
+
+            File file = new File(filePath);
+            if(file.exists()){
+                Uri putFile = Uri.fromFile(file);
+                UploadTask uploadTask = spaceRef.putFile(putFile);
+
+                // 파일 업로드의 성공/실패에 대한 콜백 받아 핸들링 하기 위해 아래와 같이 작성한다
+
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        System.out.println("======================실패"+exception.getMessage());
+                        Toast.makeText(getApplicationContext(),"파일 업로드에 실패하였습니다. ",Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                        addConsult();
+                        uploadFileCnt++;
+                        uploadDialog.setMessage("파일 전송중입니다. ("+uploadFileCnt+"/"+analysisList.size()+")");
+                        uploadDialog.show();
+                        if(uploadFileCnt==uploadList.size()){
+                            //끝나면 전문가 상담하기를 한다.
+//                            System.out.println("======================성공====");
+                            uploadDialog.dismiss();
+                            addConsult();
+                        }
+                    }
+                });
+            }
+        }
+
+
     }
 
 
